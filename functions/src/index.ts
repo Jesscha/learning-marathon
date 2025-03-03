d956c9c4893474352ab2451859e9121218d07d76
BumgeunSong <isp1195@gmail.com>
2025-03-03 14:04:17 +0900

    Telegram Update 형식에 안 맞으면 에러 던지기

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,15 +6,25 @@ import { processCommand } from './commands/process';
 // Create an Express app to handle HTTP POST requests
 const app = express();
 app.use(express.json());
-
 app.post('/webhook', async (req, res) => {
-  const update = req.body as TelegramUpdate;
-  console.log('Received webhook update:', update);
+  try {
+    const update = req.body as TelegramUpdate;
 
-  await processCommand(update);
+    // TelegramUpdate 형식이 맞는지 확인
+    if (!update.message || !update.message.chat || !update.message.from) {
+      throw new Error('Invalid TelegramUpdate format');
+    }
 
-  // Respond quickly to Telegram to acknowledge receipt
-  res.sendStatus(200);
+    console.log('Received webhook update:', update);
+
+    await processCommand(update);
+
+    // Respond quickly to Telegram to acknowledge receipt
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Error processing webhook update:', error);
+    res.status(400).send('Invalid request body');
+  }
 });
 
 // Export the Express app as a Firebase Cloud Function
